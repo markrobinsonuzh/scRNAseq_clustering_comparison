73c340214ec5586c09efc8b7d3fe617a448fbcb7
aduo <angelo.duo@bluewin.ch>
2017-09-07 10:33:14 +0200

    include silhouette

--- skript/evaluate_datasets/optimalk_sihlhouette.R
+++ skript/evaluate_datasets/optimalk_sihlhouette.R
@@ -1,5 +1,5 @@
 #############################################
-# sihlhuette plot
+# sihlouette plot
 ############################################
 
 #####################################
@@ -7,20 +7,21 @@
 #####################################
 
 ### load libraries
-source("skript/helper_files/WORKIN_DIR.R")
 
 source("skript/helper_files/Helper_functions.R")
 library(cluster)
+library(dplyr) 
+library(magrittr)
 
 
-pdf("results/plots/optimalk_wss.pdf")
+#pdf("results/plots/optimalk_sihlhoutte.pdf")
 ### set seed
 
 set.seed(1234)
 
 # Directories
 
-DATA_DIR <- paste0(WORKIN_DIR,"data")
+DATA_DIR <- "data"
 
 files <- list(
   kumar2015 = file.path(DATA_DIR, "sceset_GSE60749-GPL13112.rda"),
@@ -31,15 +32,17 @@ files <- list(
 
 # load data sets
 
-data <- vector("list", length(files))
-tinput_matrix<- data
-names(data) <- names(tinput_matrix) <-  names(files)
+list <- vector("list", length(files))
+names(list) <- names(files)
+
+list -> data -> tinput_matrix -> res.si
+
 
 for (i in names(data)){
   f <- files[[i]]
   load(f)
   data[[i]] <- res
-  
+  rm(res)
 }
 
 # extract transposed expression data
@@ -48,7 +51,32 @@ for (i in 1:(length(tinput_matrix))){
   tinput_matrix[[i]] <- t(exprs(data[[i]])) # use count scaled length scaled tpms, normalized and log2 transformed
 }
 
-#kmeans
-df <- pam(tinput_matrix[[1]], k=3)
-si <- silhouette(df)
-plot(si)
+# reduce dimension
+
+
+
+
+##########################
+# silhouette plot
+#########################
+# pam
+par.k= list(
+  kumar2015 = 3,
+  trapnell2014 = 2,
+  xue2013 = 2
+)
+
+
+for (i in names(tinput_matrix)){
+  res.si[[i]]<- silhouette(pam(tinput_matrix[[i]], k=par.k[[i]] ))
+  plot(res.si[[i]])
+}
+# kmeans
+for (i in names(tinput_matrix)){
+  res.si[[i]]<- silhouette(kmeans(tinput_matrix[[i]], k=par.k[[i]] ))
+  plot(res.si[[i]])
+}
+
+
+
+
