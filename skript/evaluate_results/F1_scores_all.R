e6101e38e34a7f0fd9122a01fb2cbb6fcc800ce5
aduo <angelo.duo@bluewin.ch>
2017-10-18 17:04:26 +0200

    change file path of helpers

--- skript/evaluate_results/F1_scores_all.R
+++ skript/evaluate_results/F1_scores_all.R
@@ -1,23 +1,20 @@
 #################################
 ### Compute the F1 scores    ####
 #################################
+# SCript loads the clustering results from the single parameter run method and calculates the the F1 score or all datasets.
+
 #load libraries
-library(dplyr)
 library(plyr)
+library(dplyr)
 library(clue)
 
 #load helper file
-source("/Users/angeloduo/Desktop/masterthesis/scRNAseq_clustering_comparison/skript/helper_files/helper_calc_f1_scores.R")
-source("/Users/angeloduo/Desktop/masterthesis/scRNAseq_clustering_comparison/skript/helper_files/calc_f1_score.R")
-source("/Users/angeloduo/Desktop/masterthesis/scRNAseq_clustering_comparison/skript/helper_files/Helper_functions.R")
+source("skript/helper_files/helper_calc_f1_scores.R")
+source("skript/helper_files/calc_f1_score.R")
+source("skript/helper_files/Helper_functions.R")
 # load data files
 DATASET <-   c("koh2016", "kumar2015", "trapnell2014", "xue2013")
-
-#DATASET <-  "kumar2015"
-
 for (h in seq_len(length(DATASET)) ){
-
-
 
 data_files <- as.list(paste0("results/run_results/",DATASET[h],".rda"))
 
@@ -39,7 +36,7 @@ names(res.f1) <- names(clusters)
 for (i in 1:length(clusters)){
   
 calc_f1_scores(clusters[[i]], clusters$labels)
-
+  
 }
 
 
@@ -58,4 +55,8 @@ calc_f1_score(labels[[i]], cluster[[i]])
 
 
 #### Appendix
-#calc_f1_scores(prd=prd,act=act)
+for (i in 1:7){
+  return(length(clusters[[6]]))
+  
+} 
+names(clusters[[7]])
