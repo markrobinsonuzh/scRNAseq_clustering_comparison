186b9899257b10a2acf58988acc3d9ca101abed6
aduo <angelo.duo@bluewin.ch>
2017-11-02 10:27:42 +0100

    cores.ratio to default

--- skript/run_methods/run_function_simlr.R
+++ skript/run_methods/run_function_simlr.R
@@ -66,7 +66,7 @@ for (i in names(data)){
   df.clus <- matrix( nrow = ncol(data[[i]]), ncol = length(par.k[[i]]) ) 
   
   for  ( j in seq_len( length( par.k[[i]]) ) ){
-  df.clus[,j] <- SIMLR(X = exprs(data[[i]]), c = par.k[[i]][j], no.dim = NA,k=10, if.impute=FALSE, normalize = FALSE)$y$cluster # use exprs slot of SCeset; log2, normalized count_lstpm
+  df.clus[,j] <- SIMLR(X = exprs(data[[i]]), c = par.k[[i]][j], no.dim = NA,k=10, if.impute=FALSE, normalize = FALSE, cores.ratio = 1)$y$cluster # use exprs slot of SCeset; log2, normalized count_lstpm
   }
   colnames(df.clus) <-  c( paste0(par.k[[i]]) )
   res.cluster[[i]] <- df.clus
