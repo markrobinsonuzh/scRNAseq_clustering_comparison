b8f92e4e5753c1699079c2a6836b35dcf5bcac4f
aduo <angelo.duo@bluewin.ch>
2017-11-15 15:07:28 +0100

    add simData

--- skript/evaluate_results/F1_scores_all.R
+++ skript/evaluate_results/F1_scores_all.R
@@ -1,24 +1,23 @@
 #################################
 ### Compute the F1 scores    ####
 #################################
-# SCript loads the clustering results from the single parameter run method and calculates the the F1 score or all datasets.
-
+# SCript loads the clustering results from the single parameter run method and calculates the the F1 score for all datasets.
+# F1 scores are stored as f1_dataset.rda  file in run_results directory
 #load libraries
 library(plyr)
 library(dplyr)
 library(clue)
 
 #load helper file
-source("skript/helper_files/helper_calc_f1_scores.R")
 source("skript/helper_files/calc_f1_score.R")
 source("skript/helper_files/Helper_functions.R")
 # load data files, define which dataset to work with
-DATASET <-   c("koh2016", "kumar2015", "trapnell2014", "zhengmix2016")
-
+DATASET <-   c("koh2016", "kumar2015", "trapnell2014", "zhengmix2016", "simDataKumar")
+# define which numbers of clusters
 ########
 for (h in seq_len(length(DATASET)) ){
 
-data_files <- as.list(paste0("results/run_results/",DATASET[h],".rda"))
+data_files <- as.list(paste0("results/run_results/cluster_single_",DATASET[h],".rda"))
 
 load(data_files[[1]])
 # reformat labels as factors
@@ -35,13 +34,6 @@ res.f1 <- vector("list", length(clusters))
 names(res.f1) <- names(clusters)
 
 
-for (i in 1:length(clusters)){
-  
-calc_f1_scores(clusters[[i]], clusters$labels)
-  
-}
-
-
 for (j in seq_len(length(res.f1))) {
   act <- clusters$labels
   prd <- clusters[[j]]
@@ -50,15 +42,10 @@ for (j in seq_len(length(res.f1))) {
 print(res.f1)
 
 
-save(res.f1, file = paste0("results/run_results/f1_",DATASET[h],".rda"))
-}
 
-calc_f1_score(labels[[i]], cluster[[i]]) 
+save(res.f1, file = paste0("results/run_results/f1_single_",DATASET[h],".rda"))
+}
 
 
 #### Appendix
-for (i in 1:7){
-  return(length(clusters[[6]]))
-  
-} 
-names(clusters[[7]])
+
