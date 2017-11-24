dd646103b8e2f18fa33598d447f8ed44dcaa2128
aduo <angelo.duo@bluewin.ch>
2017-11-24 17:38:27 +0100

    simplify

--- skript/run_methods/run_function_seurat.R
+++ skript/run_methods/run_function_seurat.R
@@ -15,49 +15,38 @@ source("FILES.R")
 
 # load data sets
 
-data <- labels <-res.cluster <-  sys.time <-  vector("list", length(files))
-
-names(data) <- names(res.cluster) <- names(labels) <- names(sys.time) <- names(files)
-
-
-for (i in names(data)){
-  f <- files[[i]]
-  load(f)
-  data[[i]] <- res
-  
-}
+data <- load_data(files, DATA_DIR )
 
 # load cell labels
-for(i in names(data)) {
-  labels[[i]] <- as.character(phenoData(data[[i]])@data$phenoid)
-}
+labels <- load_labels(data)
 
 # Seurat
 # Resolution parameter resolution, higher number gives more cluster, lower less cluster. we define as the standart by 0.8
 # k.param is the number of neirest neighbors
 # the number of PC dim to use was determined by an elbow plot and by the jackstraw function
+
 k.param <- list(
-    kumar2015 =ncol(data[[i]])* c(0.01,0.05,0.1,0.15),
-    trapnell2014 = ncol(data[[i]])*c(0.01,0.05,0.1,0.15),
-    zhengmix2016 = ncol(data[[i]])*c(0.01,0.05,0.1,0.15),
-    koh2016 = ncol(data[[i]])*c(0.01,0.05,0.1,0.15)
+    kumar2015 =ncol(data[["kumar2015"]])* c(0.01,0.05,0.1,0.15,0.2),
+    trapnell2014 = ncol(data[["trapnell2014"]])*c(0.01,0.05,0.1,0.15,0.2),
+    zhengmix2016 = ncol(data[["zhengmix2016"]])*c(0.01,0.05,0.1,0.15,0.2),
+    koh2016 = ncol(data[["koh2016"]])*c(0.01,0.05,0.1,0.15,0.2)
   )
 par.dims.use <-  list(
-  kumar2015 = 1:9,
+  kumar2015 = 1:5,
   trapnell2014 = 1:12,
   zhengmix2016 = 1:10,
   koh2016 = 1:15
 )
-
 #Seurat function
 run_seurat <- function( data, par.resolution, par.dims.use ) {
+  
   require(Seurat)
   res.cluster <-  sys.time <-  vector("list", length(files))
   names(res.cluster) <- names(sys.time) <- names(files)
-### Run SEurat
-for (i in names(data)) {
+  ### Run SEurat
+  for (i in names(data)) {
   # create Seurat object
-  data[[i]] <- CreateSeuratObject(raw.data = counts(data[[i]]), min.cells = 0, min.genes = 0, project = "test") # use raw count_lstpm
+  data[[i]] <- CreateSeuratObject(raw.data = assay(data[[i]], "counts" ), min.cells = 0, min.genes = 0, project = "test") # use raw count_lstpm
   ## Normalizing the data. After removing unwanted cells from the dataset, 
   # the next step is to normalize the data. By default, we employ a global-scaling normalization method "LogNormalize" 
   #that normalizes the gene expression measurements for each cell by the total expression, 
@@ -70,10 +59,10 @@ for (i in names(data)) {
 
 ### Perform lin dimension reduction and cluster the cells
   ### Perform linear dimensional reduction
-  sys.time[[i]] <- system.time({
+ 
     
     data[[i]] <- RunPCA(object = data[[i]], pc.genes = data[[i]]@var.genes, do.print = FALSE)
-    data[[i]] <- JackStraw(object = data[[i]], num.replicate = 100, do.print = TRUE)
+    data[[i]] <- JackStraw(object = data[[i]], num.replicate = 100, do.print = FALSE)
     JackStrawPlot(data[[i]] , PCs=1:15)
     PCElbowPlot(object =   data[[i]])
     
@@ -88,7 +77,7 @@ for (i in names(data)) {
                             print.output = 0, save.SNN = TRUE  )@ident
                            
     } 
-  })
+ 
   colnames(df.clus) <- c( paste0(  k.param[[i]]) )
   res.cluster[[i]] <-  df.clus
 }
@@ -101,8 +90,6 @@ res.cluster <-  run_seurat( data,  k.param, par.dims.use )
 # save clusters
 
 dir_cluster <- paste0("results/Seurat/seurat_krange_clus_", names(res.cluster), ".txt")
-
-
 save_clusters(res.cluster,dir_cluster)
 
 # save systemtime
