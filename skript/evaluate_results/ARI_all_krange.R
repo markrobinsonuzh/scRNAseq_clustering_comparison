1928cf971767c2339593f4bb32f2d9fdbb73f657
aduo <angelo.duo@bluewin.ch>
2017-11-24 17:37:47 +0100

    use dplyr

--- skript/evaluate_results/ARI_all_krange.R
+++ skript/evaluate_results/ARI_all_krange.R
@@ -2,20 +2,25 @@
 ### Compute the ARI for a  range of clusters k 
 ### For all Methods and datasets
 ######################################################
+#This script takes the cluster results from the k range run (cluster_krange_",DATASET[h],".rda") and computes the ARI. 
+# The results are stored as a ari_krange_DATASET.rda file.
 
+# run it per h , h for loop not working
 #load libraries
 library(dplyr)
 library(mclust)
 # load data files
-DATASET <- c("koh2016", "kumar2015", "trapnell2014", "zhengmix2016")
-for (h in seq_len(length(DATASET)) ) {
-  
+DATASET <- c( "kumar2015", "trapnell2014","koh2016" ,"zhengmix2016")
+for (h in seq_len(length(DATASET) ) ) {
+  print(DATASET[h])
   data_files <- as.list(paste0("results/run_results/cluster_krange_",DATASET[h],".rda"))
   
   load(data_files[[1]])
   # reformat labels as factors
   clusters$labels <- clusters$labels%>%as.factor
-
+  lapply(clusters, unlist) %>%lapply(as.factor) %>% lapply(levels)
+  
+  
   # create lookupfile
   #look <- lapply(clusters$labels, function(x) levels(x) <- c(1:length(levels(x))) )
   levels(clusters$labels) <- c(1:length(levels(clusters$labels)))
@@ -32,13 +37,16 @@ for (h in seq_len(length(DATASET)) ) {
     pred_i   <- unlist( clusters[[i]][j] )
     res.ari[[i]]$ARI[j] <- adjustedRandIndex(pred_i,lab_i)
     res.ari[[i]]$par <-  colnames(clusters[[i]])
-    
+    res.ari[[i]]$ncluster <-   apply(clusters[[i]], 2, function(x)length(unique(x)))
+
+      
   }
-    save(res.ari, file = paste0("results/run_results/ari_krange_",DATASET[h],".rda"))
     
-    print(res.ari)
+    
+   
   }
 
+  save(res.ari, file = paste0("results/run_results/ari_krange_",DATASET[h],".rda"))
   
   }
 
