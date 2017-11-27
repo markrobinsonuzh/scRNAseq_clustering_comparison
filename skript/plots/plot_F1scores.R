d5d5bef5e691d0e4646981f6ae93347de2075cb4
aduo <angelo.duo@bluewin.ch>
2017-11-27 16:03:38 +0100

    change dataype

--- skript/plots/plot_F1scores.R
+++ skript/plots/plot_F1scores.R
@@ -15,15 +15,16 @@ library(RColorBrewer)
 ## define the data directories
 DATA_DIR <-  "results/run_results"
 
-
+# which dataset
+datatype <- "default"
 ## read in cluster results from Rdata files
 # files directories per dataset
 files_f1 <- list(
-  kumar2015 = file.path(DATA_DIR, "f1_single_kumar2015.rda"),
-  trapnell2014 = file.path(DATA_DIR, "f1_single_trapnell2014.rda"),
-  koh2016 = file.path(DATA_DIR, "f1_single_koh2016.rda"),
-  zhengmix2016=file.path(DATA_DIR, "f1_single_zhengmix2016.rda"),
-  simDataKumar =file.path(DATA_DIR, "f1_single_simDataKumar.rda")
+  kumar2015 = file.path(DATA_DIR, paste0("f1_single_",datatype,"_kumar2015.rda") ),
+  trapnell2014 = file.path(DATA_DIR, paste0("f1_single_",datatype,"_trapnell2014.rda") ),
+  koh2016 = file.path(DATA_DIR, paste0("f1_single_",datatype,"_koh2016.rda") ),
+  zhengmix2016=file.path(DATA_DIR, paste0("f1_single_",datatype,"_zhengmix2016.rda") ),
+  simDataKumar =file.path(DATA_DIR, paste0( "f1_single_",datatype,"_simDataKumar.rda"))
 )
 
 for ( i in names(files_f1)){
@@ -34,13 +35,13 @@ tmp <- lapply(files_f1[[i]], function(x)get(load(x)))
 # create table with data , remove the column with the "ground truth" (label)
 tmp <- ldply(tmp[[1]], data.frame) %>% select(.id, f1, labels, n.cluster) %>% subset(!(.id=="labels") )
 # tmp <- daply(tmp, .(labels, .id), function(x) x$f1) old way 
-tmp <- xtabs( f1~labels+.id, tmp, addNA=TRUE )
+tmp <- xtabs( f1~labels+.id, tmp, addNA=FALSE )
 
 
 # remove NA labels
 tmp <- tmp[!is.na( rownames(tmp) ), ]
 # assign a zero to undefined entries
-tmp <- ifelse(is.na(tmp)==TRUE, 0, tmp)
+tmp <- ifelse(is.na(tmp)==TRUE, NA, tmp)
 # remove the label column from the table
 ##plot it
 pheatmap(tmp , color = colorRampPalette(brewer.pal(3, "YlGnBu"))(10),
@@ -49,7 +50,7 @@ pheatmap(tmp , color = colorRampPalette(brewer.pal(3, "YlGnBu"))(10),
          main = paste0(names(files_f1[i])), 
          width = 6, 
          height = 6,
-         number_format="%.2f", filename=paste0("results/plots/plot_f1_",names(files_f1[i]),".pdf")
+         number_format="%.2f", filename= paste0("results/plots/plot_f1_",datatype,"_",names(files_f1[i]),".pdf")
 )
          
 }
