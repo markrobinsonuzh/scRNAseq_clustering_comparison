fa1db98fc9079421f7262c0da0cebfdf72ca3ee0
aduo <angelo.duo@bluewin.ch>
2017-12-01 13:54:58 +0100

    simplify

--- skript/evaluate_results/compute_ARI_single_all.R
+++ skript/evaluate_results/compute_ARI_single_all.R
@@ -8,41 +8,8 @@ library(dplyr)
 library(mclust)
 # load data files
 DATASET <- c("koh2016", "kumar2015",  "trapnell2014","zhengmix2016", "simDataKumar")
-datatype<-  "unfiltered"
-for (h in seq_len(length(DATASET)) ) {
-  
-  data_files <- as.list(paste0("results/run_results/cluster_single_",datatype,"_",DATASET[h],".rda"))
-  
-  load(data_files[[1]])
-  
-  # reformat labels as factors
-  
-  clusters$labels <- clusters$labels%>%as.factor
-  
-  # create lookupfile
-  #look <- lapply(clusters$labels, function(x) levels(x) <- c(1:length(levels(x))) )
-  levels(clusters$labels) <- c(1:length(levels(clusters$labels)))
-  clusters$labels <- as.integer(clusters$labels)%>%as.data.frame
-  ### calculate ARI scores
-  res.ari <- vector("list", length(clusters))
-  names(res.ari) <- names(clusters)
-  
-  for (i in names(clusters)) {
-      lab_i    <- clusters$labels[,1]
-      pred_i   <- unlist( clusters[[i]])
-      res.ari[[i]]$ARI <- adjustedRandIndex(pred_i,lab_i)
+datatype<-  "default"
 
-    }
-    save(res.ari, file = paste0("results/run_results/ari_single_",datatype,"_",DATASET[h],".rda"))
-    
-    print(res.ari)
-  
-}
-
-
-#### Appendix
-DATASET <- c("koh2016", "kumar2015",  "trapnell2014","zhengmix2016", "simDataKumar")
-datatype<-  "unfiltered"
 for (h in seq_len(length(DATASET)) ) {
   
   data_files <- as.list(paste0("results/run_results/cluster_single_",datatype,"_",DATASET[h],".rda"))
