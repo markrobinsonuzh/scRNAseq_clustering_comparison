354f7178a01bde401cca4a3cb578616a2f1fb9cd
aduo <angelo.duo@bluewin.ch>
2017-12-05 18:09:09 +0100

    add methods

--- skript/run_methods/run_method/run_allv2.R
+++ skript/run_methods/run_method/run_allv2.R
@@ -2,6 +2,7 @@
 # Run all, single run scripts ##
 ################################
 rm(list=ls())
+set.seed(1234)
 # source the functions
 FILE_DIR = "skript/run_methods/run_method/"
 
@@ -10,50 +11,63 @@ source( paste0(FILE_DIR , "meth_linnormv2.R") ) #ok
 source( paste0(FILE_DIR , "meth_pcaReducev2.R") ) #ok
 source( paste0(FILE_DIR , "meth_RtSNEkmeansv2.R") ) #ok
 source( paste0(FILE_DIR , "meth_SIMLR_largescalev2.R") ) #ok
+source( paste0(FILE_DIR , "meth_SIMLRv2.R") ) #ok
+
 source( paste0(FILE_DIR , "meth_TSCANv2.R") ) #ok
 source( paste0(FILE_DIR , "meth_RaceIDv2.R") ) #ok
+source( paste0(FILE_DIR , "meth_zinbwavev2.R") ) #DLL fulll
+
 source( paste0(FILE_DIR , "meth_seuratv2.R") ) #DLL fulll
 source( paste0(FILE_DIR , "meth_SC3v2.R") ) # DLL fulll
 # run the methods on the respetive datasets
+# which dataset "kumar2015"  ,  "trapnell2014" ,"zhengmix2016" ,"koh2016"  ,"simDataKumar"
+dataset <- c("kumar2015"  ,  "trapnell2014" ,"zhengmix2016" ,"koh2016"  ,"simDataKumar" )
 # on defualt
 datatype <- "default"
-analyze_cidr(datatype)
-analyze_linnorm(datatype)
-analyze_pcareduce(datatype)
-analyze_rtsnekmeans(datatype) #error
-analyze_simlrlargescale(datatype)
-analyze_tscan(datatype)
-analyze_raceid(datatype)
-analyze_seurat(datatype)
 
-analyze_sc3(datatype)
+#run
+analyze_cidr(datatype, dataset)
+analyze_linnorm(datatype, dataset)
+analyze_pcareduce(datatype,dataset)
+analyze_rtsnekmeans(datatype, dataset) #error
+analyze_simlrlargescale(datatype, dataset)
+analyze_simlr(datatype, dataset)
+analyze_tscan(datatype, dataset)
+analyze_raceid(datatype, dataset)
+analyze_seurat(datatype, dataset)
+analyze_zinbwave(datatype, dataset)
+
+ analyze_sc3(datatype, dataset)
 
 
 # on filtered
 datatype <- "filtered"
-analyze_cidr(datatype)
-analyze_linnorm(datatype)
-analyze_pcareduce(datatype)
-analyze_raceid(datatype)
-analyze_rtsnekmeans(datatype) #error
-analyze_simlrlargescale(datatype)
-analyze_tscan(datatype)
-analyze_seurat(datatype)
-
-analyze_sc3(datatype)
+analyze_cidr(datatype, dataset)
+analyze_linnorm(datatype, dataset)
+analyze_pcareduce(datatype,dataset)  
+analyze_rtsnekmeans(datatype, dataset) 
+analyze_simlrlargescale(datatype, dataset)
+analyze_tscan(datatype, dataset)
+analyze_raceid(datatype, dataset)
+analyze_seurat(datatype, dataset)
+analyze_zinbwave(datatype, dataset)
+analyze_sc3(datatype, dataset)
 
 # on unfiltered
 datatype <- "unfiltered"
-analyze_cidr(datatype)
-analyze_linnorm(datatype)
-analyze_pcareduce(datatype)
-analyze_raceid(datatype)
-analyze_rtsnekmeans(datatype) #error
-analyze_simlrlargescale(datatype)
-analyze_tscan(datatype)
-analyze_seurat(datatype)
+analyze_cidr(datatype, dataset)
+analyze_linnorm(datatype, dataset)
+analyze_pcareduce(datatype,dataset)
+analyze_rtsnekmeans(datatype, dataset) #error
+analyze_simlrlargescale(datatype, dataset)
+analyze_simlr(datatype, dataset)
 
-analyze_sc3(datatype)
+analyze_tscan(datatype, dataset)
+analyze_raceid(datatype, dataset)
+analyze_seurat(datatype, dataset)
+analyze_zinbwave(datatype, dataset)
+
+analyze_sc3(datatype, dataset)
 
 
 
