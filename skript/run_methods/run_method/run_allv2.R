286eaab1e7afdd999a195d1e8ada8730b38621a6
aduo <angelo.duo@bluewin.ch>
2018-01-25 16:12:04 +0100

    no changes

--- skript/run_methods/run_method/run_allv2.R
+++ skript/run_methods/run_method/run_allv2.R
@@ -6,22 +6,21 @@ set.seed(1234)
 # source the functions
 FILE_DIR = "skript/run_methods/run_method/"
 
-source( paste0(FILE_DIR , "meth_cidrv2.R") ) #ok
-source( paste0(FILE_DIR , "meth_linnormv2.R") ) #ok
-source( paste0(FILE_DIR , "meth_pcaReducev2.R") ) #ok
-source( paste0(FILE_DIR , "meth_RtSNEkmeansv2.R") ) #ok
-source( paste0(FILE_DIR , "meth_SIMLR_largescalev2.R") ) #ok
-source( paste0(FILE_DIR , "meth_SIMLRv2.R") ) #ok
 
-source( paste0(FILE_DIR , "meth_TSCANv2.R") ) #ok
-source( paste0(FILE_DIR , "meth_RaceIDv2.R") ) #ok
-source( paste0(FILE_DIR , "meth_zinbwavev2.R") ) #DLL fulll
-
-source( paste0(FILE_DIR , "meth_seuratv2.R") ) #DLL fulll
-source( paste0(FILE_DIR , "meth_SC3v2.R") ) # DLL fulll
+source( paste0(FILE_DIR , "meth_cidrv2.R") ) 
+source( paste0(FILE_DIR , "meth_linnormv2.R") ) 
+source( paste0(FILE_DIR , "meth_pcaReducev2.R") ) 
+source( paste0(FILE_DIR , "meth_RtSNEkmeansv2.R") ) 
+source( paste0(FILE_DIR , "meth_SIMLR_largescalev2.R") ) 
+source( paste0(FILE_DIR , "meth_SIMLRv2.R") ) 
+source( paste0(FILE_DIR , "meth_TSCANv2.R") ) 
+source( paste0(FILE_DIR , "meth_RaceIDv2.R") ) 
+source( paste0(FILE_DIR , "meth_zinbwavev2.R") ) 
+source( paste0(FILE_DIR , "meth_seuratv2.R") ) 
+source( paste0(FILE_DIR , "meth_SC3v2.R") ) 
 # run the methods on the respetive datasets
-# which dataset "kumar2015"  ,  "trapnell2014" ,"zhengmix2016" ,"koh2016"  ,"simDataKumar"
-dataset <- c("kumar2015"  ,  "trapnell2014" ,"zhengmix2016" ,"koh2016"  ,"simDataKumar" )
+# which dataset "kumar2015"  ,  "trapnell2014" ,"zhengmix2016" ,"koh2016"  ,"simDataKumar", "simDataKumar2"
+dataset <- c( "kumar2015"  ,  "trapnell2014" ,"zhengmix2016" ,"koh2016"  ,"simDataKumar", "simDataKumar2"  )
 # on defualt
 datatype <- "default"
 
@@ -29,7 +28,7 @@ datatype <- "default"
 analyze_cidr(datatype, dataset)
 analyze_linnorm(datatype, dataset)
 analyze_pcareduce(datatype,dataset)
-analyze_rtsnekmeans(datatype, dataset) #error
+analyze_rtsnekmeans(datatype, dataset) 
 analyze_simlrlargescale(datatype, dataset)
 analyze_simlr(datatype, dataset)
 analyze_tscan(datatype, dataset)
@@ -37,6 +36,7 @@ analyze_raceid(datatype, dataset)
 analyze_seurat(datatype, dataset)
 analyze_zinbwave(datatype, dataset)
 analyze_sc3(datatype, dataset)
+
 
 
 # on filtered
@@ -47,7 +47,6 @@ analyze_pcareduce(datatype,dataset)
 analyze_rtsnekmeans(datatype, dataset) 
 analyze_simlrlargescale(datatype, dataset)
 analyze_simlr(datatype, dataset)
-
 analyze_tscan(datatype, dataset)
 analyze_raceid(datatype, dataset)
 analyze_seurat(datatype, dataset)
@@ -62,15 +61,13 @@ analyze_pcareduce(datatype,dataset)
 analyze_rtsnekmeans(datatype, dataset) 
 analyze_simlrlargescale(datatype, dataset)
 analyze_simlr(datatype, dataset)
-
 analyze_tscan(datatype, dataset)
 analyze_raceid(datatype, dataset)
 analyze_seurat(datatype, dataset)
 analyze_zinbwave(datatype, dataset)
-
 analyze_sc3(datatype, dataset)
 
-# on unfiltered
+# on optimal k
 datatype <- "optimalk"
 analyze_cidr(datatype, dataset)
 analyze_linnorm(datatype, dataset)
@@ -97,4 +94,5 @@ analyze_raceid(datatype, dataset)
 analyze_seurat(datatype, dataset)
 analyze_zinbwave(datatype, dataset)
 analyze_sc3(datatype, dataset)
+
 
