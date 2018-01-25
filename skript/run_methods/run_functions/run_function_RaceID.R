4f6fefcb3640c6b14c5228e1541f09e1429864ac
aduo <angelo.duo@bluewin.ch>
2018-01-25 17:58:55 +0100

    add parameters

--- skript/run_methods/run_functions/run_function_RaceID.R
+++ skript/run_methods/run_functions/run_function_RaceID.R
@@ -6,7 +6,7 @@
 # The method is based on transcript counts obtained with unique molecular identifies.
 
 
-run_function_raceid <- function( data, labels, datatype ,par.mintotal, par.maxexpr,do.gap,cln ) {
+run_function_raceid <- function( data, labels, datatype ,par.mintotal, par.maxexpr,par.minexprs, par.minnumber,do.gap,cln ) {
   
   
   source("method_resources/RaceID/RaceID_class.R")
@@ -22,6 +22,7 @@ run_function_raceid <- function( data, labels, datatype ,par.mintotal, par.maxex
   require(amap)
   require(RColorBrewer) 
   require(locfit)
+  require(dplyr)
 
 # create store vectors
 list<- vector("list", length(data))
@@ -31,8 +32,8 @@ for (i in names(data)){
   print(i)
   sys.time [[i]] <- system.time({
     sc[[i]] <- SCseq(as.data.frame(counts(data[[i]]))) # extract the expression data
-    sc[[i]] <- filterdata(sc[[i]], mintotal = par.mintotal[[i]], minexpr = 5, 
-                     minnumber = 1, maxexpr = par.maxexpr[[i]] , downsample = FALSE, dsn = 1, rseed = 1234)
+    sc[[i]] <- filterdata(sc[[i]], mintotal = par.mintotal[[i]], minexpr = par.minexprs[[i]], 
+                     minnumber = par.minnumber[[i]], maxexpr = par.maxexpr[[i]] , downsample = FALSE, dsn = 1, rseed = 1234)
     
     res.cluster[[i]]<- clustexp(sc[[i]], metric="pearson", cln=cln[[i]], do.gap=do.gap[[i]], clustnr=20, B.gap=50, SE.method="Tibs2001SEmax", 
                    SE.factor=.25, bootnr=50, rseed=1234)@kmeans$kpart
@@ -53,10 +54,22 @@ dir_systime <-  paste0("results/",datatype,"/raceid/raceid_systime_",names(sys.t
 
 save_systemtime(sys.time, dir_systime)
 
-# save experiment labels
 
+# save experiment labels
+# merge labels
+ l <- NULL
+ x<- NULL
+ list<- vector("list", length(data))
+ names(list) <- names(data)
+ list->l ->x
+for (i in names(labels)){
+  l[[i]] <- colData(data[[i]])$phenoid%>%as.data.frame
+  rownames(l[[i]]) <- colnames(data[[i]])
+  x[[i]] <- l[[i]][ names(res.cluster[[i]]),]
+}
+x <- lapply(x, as.character)
 dir_labels <-  paste0("results/",datatype,"/raceid/raceid_labels_",names(labels), ".txt")
-save_labels(labels, dir_labels )
+save_labels(x, dir_labels )
 
 
 ###### Save Session Info
@@ -65,3 +78,4 @@ sessionInfo()
 sink()
 
 }
+#
