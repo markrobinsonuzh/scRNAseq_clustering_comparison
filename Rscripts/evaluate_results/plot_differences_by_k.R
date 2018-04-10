80ef46b1dd6fb80939c5128d81b86756f449ce11
aduo <angelo.duo@bluewin.ch>
2018-04-10 15:36:40 +0200

    change dir

--- Rscripts/evaluate_results/plot_differences_by_k.R
+++ Rscripts/evaluate_results/plot_differences_by_k.R
@@ -34,35 +34,17 @@ res_summary <- res %>% dplyr::group_by(dataset,method, run, k) %>%
 diff_abs3 <- res_summary %>% dplyr::group_by(dataset,filtering,method, truenclust,k) %>%summarize(medARI=median(ARI)) %>%filter(medARI==max(medARI)) %>%mutate(k_diff= (k-truenclust))
 
 
-print(ggplot(diff_abs3, aes(x = method, y = k_diff, group = method, color = method)) + 
-        geom_point() + 
-        theme_bw() +
-        scale_color_brewer(palette = "Set3")+
-        theme(axis.text.x = element_text(size=rel(0.8),angle = 90, hjust = 1, vjust = 1)) )
-
-
-pdf("../../plots/performance/diff_maxARI.pdf")
+pdf("../../plots/performance/difference_in_k.pdf", height = 10, width=8)
 print(ggplot(diff_abs3, aes(x = method, y = k_diff, group = method, color = method)) + 
         geom_boxplot( outlier.color = NA) + 
         geom_point(position = position_jitter(width = 0.1), alpha = 0.1)+
         theme_bw() +
         scale_color_brewer(palette = "Set3")+
+        labs(title = "difference_maxARI")+
         theme(axis.text.x = element_text(size=rel(1),angle = 90, hjust = 1, vjust = 1)) )
-dev.off()
 
-print(ggplot(diff_abs3, aes(x = method, y = abs(k_diff), group = method, color = method)) + 
-        geom_boxplot( outlier.color = NA) + 
-        geom_point(position = position_jitter(width = 0.1), alpha = 0.1)+
-        theme_bw() +
-        scale_color_brewer(palette = "Set3")+
-        theme(axis.text.x = element_text(size=rel(1),angle = 90, hjust = 1, vjust = 1)) )
  
- ggplot(diff_abs3) +
-  geom_boxplot(aes(x = method, y = k_diff, group = method, color = method),
-               notch = FALSE, 
-               outlier.color = "black")+
-                 geom_point(aes(x = method, y = k_diff, group = method, color = method),
-               position = position_jitter(width = 0.1), alpha = 0.1)
+ 
 # absolute values orn not
  
 diff_med <- res_summary %>% dplyr::group_by(dataset,filtering,method, k) %>%summarize(medianARI= median(ARI))%>%ungroup()%>%dplyr::group_by(dataset,filtering,method, k)%>%
@@ -76,12 +58,13 @@ diff_abs <- res_summary %>% dplyr::group_by(dataset,filtering,method, run) %>% f
 diff_estnclust <- res_summary %>%dplyr::group_by(dataset,filtering,method,estnclust,truenclust) %>%
   summarize(medARI=median(ARI)) %>%
   filter(medARI==max(medARI))%>%filter(estnclust != is.na(estnclust))%>%mutate(k_diff= (estnclust-truenclust))
-pdf("../../plots/performance/diff_estnclust.pdf")
 diff_estnclust <- res_summary %>%  filter(estnclust != is.na(estnclust))%>%group_by(method, dataset, filtering, estnclust, truenclust)%>%summarise(k_diff= unique(estnclust)- unique(truenclust))
 print(ggplot(diff_estnclust, aes(x = method, y = k_diff, group = method, color = method)) + 
         geom_boxplot( outlier.color = NA) + 
         geom_point(position = position_jitter(width = 0.1), alpha = 0.1)+
         theme_bw() +
+        labs(title = "difference_estnclust")+
         scale_color_brewer(palette = "Set3")+
         theme(axis.text.x = element_text(size=rel(1),angle = 90, hjust = 1, vjust = 1)) )
 dev.off()
+
