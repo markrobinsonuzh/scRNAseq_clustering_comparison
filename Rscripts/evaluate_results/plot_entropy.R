1e1e28d2d35e89e7f99b71eee1f68c52a9c3ef75
aduo <angelo.duo@bluewin.ch>
2018-05-17 14:49:09 +0200

    add marker for true entropy

--- Rscripts/evaluate_results/plot_entropy.R
+++ Rscripts/evaluate_results/plot_entropy.R
@@ -11,32 +11,44 @@ suppressPackageStartupMessages({
 
 res <-  readRDS(file="output/clustering_summary/clustering_summary.rds")
 
-entropy <- function(cluster){
+shanon_entropy <- function(cluster){
   p <-c(table(cluster)) / length(cluster)
   s <- -1*sum(p*log2(p))
   return(s)
 }
 
 # ------------------------------------
-# compute entropy
+# compute Entropy, 
 # ------------------------------------
-res_summary <- res %>% dplyr::group_by(dataset, method, run, k) %>% dplyr::filter(!is.na(cluster)) %>% 
-  dplyr::summarize(s = entropy(cluster),
+res_summary <- res %>% dplyr::group_by( dataset, method, run, k) %>% dplyr::filter(!is.na(cluster)) %>% 
+  dplyr::summarize(s = shanon_entropy(cluster),
+                   s.true= shanon_entropy(trueclass),
                    truenclust = length(unique(trueclass)),
                    estnclust = unique(est_k)
                    )  %>%
-  tidyr::separate(dataset, sep = "_", into = c("sce", "filtering", "dataset")) %>%
+  tidyr::separate( dataset, sep = "_", into = c("sce", "filtering", "dataset")) %>%
+  dplyr::select(-sce) %>% dplyr::ungroup()
+# ------------------------------------
+# compute Entropy, for truenclust 
+# ------------------------------------
+res_summary <- res %>% dplyr::group_by(dataset, method, run, k) %>% dplyr::filter(!is.na(cluster)) %>% 
+  dplyr::summarize(s = shanon_entropy(cluster),
+                   truenclust = length(unique(trueclass)),
+                   estnclust = unique(est_k)
+  )  %>%
+  tidyr::separate( dataset, sep = "_", into = c("sce", "filtering", "dataset")) %>%
   dplyr::select(-sce) %>% dplyr::ungroup()
 
-# ------------------------------------
-# plot entropy per k
+res %>% group_by( dataset, method, run, k ) %>% dplyr::summarize( s.true = shanon_entropy(trueclass) )
+  # plot entropy per k
 # ------------------------------------
 pdf("plots/performance/plot_entropy_by_k.pdf", width = 12, height = 6)
 
 print( ggplot(data = res_summary%>%filter( !is.na(s)), aes(x = k, y = s, group=method, color=method))+       
          geom_smooth()+  
          facet_grid(filtering~dataset)+
-         geom_vline(aes(xintercept = truenclust), linetype = "dashed") + 
+         geom_vline(aes(xintercept = truenclust), linetype = "dashed")+ 
+         geom_point(aes( truenclust , s.true ), color=1, shape=4)+
          scale_color_brewer(palette = "Set3")+
          theme_bw()+
          labs(x="method", y="entropy") 
@@ -44,4 +56,5 @@ print( ggplot(data = res_summary%>%filter( !is.na(s)), aes(x = k, y = s, group=m
 dev.off()
 date()
 sessionInfo()
+
 
