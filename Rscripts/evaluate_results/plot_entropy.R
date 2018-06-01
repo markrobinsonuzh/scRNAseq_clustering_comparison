7f583d5cccbe80860ce25f627f58333e6276080c
aduo <angelo.duo@bluewin.ch>
2018-06-01 17:29:25 +0200

    add normalised entropy

--- Rscripts/evaluate_results/plot_entropy.R
+++ Rscripts/evaluate_results/plot_entropy.R
@@ -22,21 +22,24 @@ shanon_entropy <- function(cluster){
 # ------------------------------------
 # compute Entropy and ARi 
 # ------------------------------------
-res_summary <- res %>% dplyr::group_by( dataset, method, run, k) %>% dplyr::filter(!is.na(cluster)) %>% 
+res_entropy <- res %>% 
+  dplyr::group_by( dataset, method, run, k) %>% 
+  dplyr::filter(!is.na(cluster)) %>% 
   dplyr::summarize(s = shanon_entropy(cluster),
-                   ARI = mclust::adjustedRandIndex(cluster, trueclass),
                    s.true=  shanon_entropy(trueclass),
+                   s.true.norm = s.true/log2(unique(k)),
+                   s.norm=s/log2(unique(k)),
+                   ARI = mclust::adjustedRandIndex(cluster, trueclass),
                    truenclust = length(unique(trueclass)),
                    estnclust = unique(est_k)
                    )  %>%
   tidyr::separate( dataset, sep = "_", into = c("sce", "filtering", "dataset")) %>%
   dplyr::select(-sce) %>% dplyr::ungroup()
-
 # plot entropy per k
 # ------------------------------------
 pdf("plots/performance/plot_entropy_by_k.pdf", width = 12, height = 6)
 
-print( ggplot(data = res_summary%>%filter( !is.na(s)), aes(x = k, y = s, group=method, color=method))+       
+print( ggplot(data = res_entropy%>%filter( !is.na(s)), aes(x = k, y = s, group=method, color=method))+       
          geom_smooth()+  
          facet_grid(filtering~dataset, scale="free_x")+
          geom_vline(aes(xintercept = truenclust), linetype = "dashed")+ 
@@ -45,7 +48,7 @@ print( ggplot(data = res_summary%>%filter( !is.na(s)), aes(x = k, y = s, group=m
          theme_bw()+
          labs(x="method", y="entropy") 
 )
-print( ggplot(data = res_summary%>%filter(  k==truenclust), 
+print( ggplot(data = res_entropy%>%filter(  k==truenclust), 
               aes(x = ARI, y = s, group=method, color=method))+       
          geom_point()+  
          facet_grid(filtering~dataset, scale="free")+
@@ -54,6 +57,60 @@ print( ggplot(data = res_summary%>%filter(  k==truenclust),
          manual.scale+
          theme_bw()+
          labs(x="ARI", y="entropy") 
+)
+print( ggplot(data = res_entropy%>%filter(  k==truenclust), 
+              aes(x = ARI, y = s, group=method, color=method))+       
+         geom_point()+  
+         facet_grid(filtering~dataset, scale="free")+
+         geom_hline(aes(yintercept = s.true), linetype = "dashed")+ 
+         #geom_point(aes( x=truenclust , y=s.true ), color=1, shape=4)+
+         manual.scale+
+         theme_bw()+
+         labs(x="ARI", y="entropy") 
+)
+print( ggplot(data = res_entropy%>%filter( k==truenclust), 
+              aes(x = method, y = s.norm, group=method, color=method))+       
+         geom_boxplot()+  
+         facet_grid(dataset~., scale="free")+
+         geom_hline(aes(yintercept = s.true.norm), linetype = "dashed")+ 
+         #geom_point(aes( x=truenclust , y=s.true ), color=1, shape=4)+
+         manual.scale+
+         theme_bw()+
+         labs(x="method", y="norm.entropy", title= "normalised entropy per dataset, k=truenclust") 
+)
+
+print( ggplot(data = res_entropy%>%filter( k==truenclust), 
+              aes(x = reorder(method, s.norm, FUN=mean),
+                              y = s.norm, group=method, color=method))+       
+         geom_boxplot()+  
+         #facet_grid(dataset~., scale="free")+
+         #geom_hline(aes(yintercept = s.true.norm), linetype = "dashed")+ 
+         #geom_point(aes( x=truenclust , y=s.true ), color=1, shape=4)+
+         manual.scale+
+         theme_bw()+
+         labs(x="method", y="norm.entropy", title= "normalised entropy all datasets, k=truenclust" ) 
+)
+print( ggplot(data = res_entropy, 
+              aes(x = k,
+                  y = s.norm, group=method, color=method))+       
+         geom_smooth()+  
+         #facet_grid(dataset~., scale="free")+
+         #geom_hline(aes(yintercept = s.true.norm), linetype = "dashed")+ 
+         #geom_point(aes( x=truenclust , y=s.true ), color=1, shape=4)+
+         manual.scale+
+         theme_bw()+
+         labs(x="method", y="norm.entropy", title= "normalised entropy all datasets, by k" ) 
+)
+print( ggplot(data = res_entropy, 
+              aes(x = reorder(method, s.norm, FUN=median, na.rm=TRUE),
+                  y = s.norm, group=method, color=method))+       
+         geom_boxplot()+  
+         #facet_grid(dataset~., scale="free")+
+         #geom_hline(aes(yintercept = s.true.norm), linetype = "dashed")+ 
+         #geom_point(aes( x=truenclust , y=s.true ), color=1, shape=4)+
+         manual.scale+
+         theme_bw()+
+         labs(x="method", y="norm.entropy", title= "normalised entropy all datasets, all k" ) 
 )
 
 dev.off()
