7f35c3073be24650aa6dc771f3ae851a42c64b2e
aduo <angelo.duo@bluewin.ch>
2018-06-04 12:42:53 +0200

    estnclust for each run

--- Rscripts/evaluate_results/plot_differences_by_k.R
+++ Rscripts/evaluate_results/plot_differences_by_k.R
@@ -36,10 +36,10 @@ res_summary <- res %>% dplyr::group_by(dataset,method, run, k) %>%
 
 # difference in k to maximum of median ARI (median by method and k)
 diff_abs <- res_summary %>% 
-  dplyr::group_by(dataset,filtering,method, truenclust,k) %>%
-  summarize(medARI=median(ARI)) %>%
-  filter(medARI==max(medARI)) %>%
-  mutate(k_diff= (k-truenclust))
+  dplyr::group_by(dataset,filtering,method, truenclust, k) %>%
+  dplyr::summarize(medARI=median(ARI)) %>%
+  dplyr::filter(medARI==max(medARI)) %>%
+  dplyr::mutate(k_diff= (k-truenclust))
 
 
 pdf("plots/performance/difference_in_k.pdf", height = 15, width=8)
@@ -64,25 +64,29 @@ p2 <- ggplot(diff_abs, aes(x = method, y = k_diff, group = method, color = metho
 
 #  difference to estimated number of clusters
 
-diff_estnclust <- res_summary %>%  filter(estnclust != is.na(estnclust))%>%group_by(method, dataset, filtering, estnclust, truenclust)%>%
-  summarise(k_diff= unique(estnclust)- unique(truenclust))
+diff_estnclust <- res_summary %>%  
+  dplyr::filter(k==truenclust)%>%
+  group_by(method, dataset, filtering, estnclust, truenclust)%>%
+  dplyr::mutate(k_diff= estnclust- truenclust)
+diff_estnclust$method <- as.factor( diff_estnclust$method )
 
 p3 <- ggplot(diff_estnclust, aes(x = method, y = k_diff, group = method, color = method)) + 
         geom_boxplot( outlier.color = NA) + 
-        geom_point( alpha = 0.8)+
+        geom_dotplot( binaxis = "y", stackdir = "center",  dotsize = 0.1, stackratio=1)+
         theme_bw() +
         labs(title = "difference_estnclust")+
         manual.scale+
+        #ggrepel::geom_text_repel(aes(label = dataset),size = 2, color=1)+
         theme(axis.text.x = element_text(size=rel(1),angle = 90, hjust = 1, vjust = 1))+
         #geom_text(aes(label=dataset), nudge_x = 0.0, nudge_y = 0.0, check_overlap = T,  size=3)+
-        facet_wrap(~filtering, scales = "free")+
-        ggrepel::geom_text_repel(aes(label = dataset),
-                           size = 2, color=1) 
+        facet_wrap(~filtering, scales = "free")
 
-position = position_jitter(width = 0.1),
+
+
 dev.off()
 ggsave( filename = "plots/performance/difference_in_k.pdf",
-        cowplot::plot_grid( p1, p3, labels="auto" , ncol=1)
+        cowplot::plot_grid( p1, p3,p5, labels="auto" , ncol=1)
 )
         
         
+
