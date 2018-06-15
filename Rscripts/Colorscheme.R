4479650c7070cf2fdec6ebcd47b0a3344181585e
aduo <angelo.duo@bluewin.ch>
2018-06-15 11:04:40 +0200

    change color tscan

--- Rscripts/Colorscheme.R
+++ Rscripts/Colorscheme.R
@@ -7,10 +7,11 @@ library(RColorBrewer)
 library(ggplot2)
 # methods, add additional methods here
 methods <- c("CIDR" , "FlowSOM" , "PCAHC", "PCAKmeans","pcaReduce","RaceID", "RtsneKmeans",
-             "SC3" ,    "SC3svm" , "Seurat"  ,  "SIMLR"  ,  "TSCAN"   ) 
+             "SC3" ,    "SC3svm" , "Seurat"  ,    "TSCAN"   ) 
 
 # color set , from https://www.r-bloggers.com/the-paul-tol-21-color-salute/
-tol12qualitative=c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#AA4466", "#882255", "#AA4499")
+tol12qualitative=c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677",
+                   "grey34",  "orange")
 #set 3
 #colors <- brewer.pal(length(methods) ,"Set3")
 colors <- tol12qualitative
