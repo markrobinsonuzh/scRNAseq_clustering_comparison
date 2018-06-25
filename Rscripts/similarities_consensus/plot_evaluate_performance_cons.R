f3f7aed0f1167c267f8aec84dccb6d3deac4f598
Charlotte Soneson <charlottesoneson@gmail.com>
2018-06-25 11:40:24 +0200

    Clean up consensus performance plot

--- Rscripts/similarities_consensus/plot_evaluate_performance_cons.R
+++ Rscripts/similarities_consensus/plot_evaluate_performance_cons.R
@@ -1,8 +1,10 @@
-
 args <- (commandArgs(trailingOnly = TRUE))
 for (i in 1:length(args)) {
   eval(parse(text = args[[i]]))
 }
+
+print(consensusrds)
+print(outrds)
 
 suppressPackageStartupMessages({
   library(dplyr)
@@ -13,32 +15,34 @@ suppressPackageStartupMessages({
   library(RColorBrewer)
   library(viridis)
   library(ggthemes)
-  
 })
-source("Colorscheme.R")
+
+source("Rscripts/Colorscheme.R")
+
 # ----------------------------------------------
-# Plot summary of consensus clusterin, for control
+# Plot summary of consensus clustering, for control
 # ----------------------------------------------
 
 ## Read clustering results
-res <- readRDS(file="output/consensus/consensus_clue.rds")
-pdf("plots/performance/res_performance_cons.pdf", width=20, height = 15)
+res <- readRDS(file = consensusrds)
+
+pdf(gsub("rds$", "pdf", outrds), width = 20, height = 15)
+
 # ------------------------------------
 # compute ARI, no of unique clusters, no of estimated k, median time
 # ------------------------------------
-res_summary <- res %>% dplyr::group_by(dataset,method, run, k) %>%
-  dplyr::summarize(ARI = mclust::adjustedRandIndex(consensus.clue , trueclass),
+res_summary <- res %>% dplyr::group_by(dataset, method, run, k) %>%
+  dplyr::summarize(ARI = mclust::adjustedRandIndex(consensus, trueclass),
                    truenclust = length(unique(trueclass)),
-                   estnclust = unique(est_k))%>%
+                   estnclust = unique(est_k)) %>%
   tidyr::separate(dataset, sep = "_", into = c("sce", "filtering", "dataset")) %>%
   dplyr::select(-sce) %>% dplyr::ungroup()
+
 # --------------------------------------
 # ## Calculate performance indices for each method and clustering run
 # --------------------------------------
-# remove the method Seurat in Zheng
-
 print(ggplot(res_summary, 
-        aes(x = k, y = ARI, group = method, color = method)) + 
+             aes(x = k, y = ARI, group = method, color = method)) + 
         geom_vline(aes(xintercept = truenclust), linetype = "dashed") + 
         geom_line(size=1) + 
         theme_bw() +
@@ -49,51 +53,52 @@ print(ggplot(res_summary,
 # --------------------------------------
 # ## Heatmap  ARI of truenclust, https://github.com/hrbrmstr/facetedcountryheatmaps
 # --------------------------------------
-
 # on true k , ARI, ordered by median
-print(  p1 <- res_summary %>% dplyr::filter(k == truenclust) %>%
-          dplyr::group_by(dataset, filtering, method, k) %>%
-          ggplot(aes(x = reorder(method,ARI, median , na.rm=FALSE), y = dataset, fill = ARI))+
-          geom_tile(color="white", size=0.1, na.rm =FALSE)+
-          facet_wrap(~ filtering) +
-          scale_fill_viridis(name="ARI", direction=-1 )+
-          theme_tufte(base_family="Helvetica")+
-          labs(x=NULL, y=NULL, title="consensus ARI, k = truenclust") +
-          coord_equal() +
-          theme(axis.text.x=element_text(size=12, angle=90))+
-          theme(axis.text.y=element_text(size=12))+
-          theme(legend.title=element_text(size=12))+
-          theme(legend.title.align=1)+
-          theme(legend.text=element_text(size=11))+
-          theme(legend.position="right")+
-          theme(legend.key.size=unit(2, "cm"))+
-          theme(legend.key.width=unit(0.5, "cm"))+
-          theme(axis.ticks=element_blank())
+print(res_summary %>% dplyr::filter(k == truenclust) %>%
+        dplyr::group_by(dataset, filtering, method, k) %>%
+        ggplot(aes(x = reorder(method, ARI, median, na.rm=FALSE), y = dataset, fill = ARI)) +
+        geom_tile(color = "white", size = 0.1, na.rm = FALSE) +
+        facet_wrap(~ filtering) +
+        scale_fill_viridis(name = "ARI", direction = -1 ) +
+        theme_tufte(base_family = "Helvetica") +
+        labs(x = NULL, y = NULL, title = "consensus ARI, k = truenclust") +
+        coord_equal() +
+        theme(axis.text.x = element_text(size = 12, angle = 90),
+              axis.text.y = element_text(size = 12),
+              legend.title = element_text(size = 12),
+              legend.title.align = 1,
+              legend.text = element_text(size = 11),
+              legend.position = "right",
+              legend.key.size = unit(2, "cm"),
+              legend.key.width = unit(0.5, "cm"),
+              axis.ticks = element_blank())
 )
-# on estimated k,  ARI
 
-print(  p2 <- res_summary %>% dplyr::filter(k == estnclust) %>%
-          dplyr::group_by(dataset, filtering, method, k) %>%
-          ggplot(aes(x = reorder(method,ARI, median , na.rm=FALSE), y = dataset, fill = ARI))+
-          geom_tile(color="white", size=0.1)+
-          facet_wrap(~ filtering) +
-          scale_fill_viridis(name="ARI", direction=-1, na.value = "grey")+
-          theme_tufte(base_family="Helvetica")+
-          labs(x=NULL, y=NULL, title="Consensus ARI, k = estnclust") +
-          coord_equal() +
-          theme(axis.text.x=element_text(size=12, angle=90))+
-          theme(axis.text.y=element_text(size=12))+
-          theme(panel.border=element_blank())+
-          theme(legend.title=element_text(size=12))+
-          theme(legend.title.align=1)+
-          theme(legend.text=element_text(size=11))+
-          theme(legend.position="right")+
-          theme(legend.key.size=unit(2, "cm"))+
-          theme(legend.key.width=unit(0.5, "cm"))+
-          theme(axis.ticks=element_blank())
+# on estimated k,  ARI
+print(res_summary %>% dplyr::filter(k == estnclust) %>%
+        dplyr::group_by(dataset, filtering, method, k) %>%
+        ggplot(aes(x = reorder(method, ARI, median, na.rm = FALSE), y = dataset, fill = ARI)) +
+        geom_tile(color = "white", size = 0.1) +
+        facet_wrap(~ filtering) +
+        scale_fill_viridis(name = "ARI", direction = -1, na.value = "grey") +
+        theme_tufte(base_family = "Helvetica") +
+        labs(x = NULL, y = NULL, title = "Consensus ARI, k = estnclust") +
+        coord_equal() +
+        theme(axis.text.x = element_text(size = 12, angle = 90),
+              axis.text.y = element_text(size = 12),
+              panel.border = element_blank(),
+              legend.title = element_text(size = 12),
+              legend.title.align = 1,
+              legend.text = element_text(size = 11),
+              legend.position = "right",
+              legend.key.size = unit(2, "cm"),
+              legend.key.width = unit(0.5, "cm"),
+              axis.ticks = element_blank())
 )
 
 dev.off()
+
+saveRDS(NULL, file = outrds)
 date()
 sessionInfo()
 
