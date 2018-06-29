f4642c55675f6c37e77e2d9582f1e4ffc3e32ff8
aduo <angelo.duo@bluewin.ch>
2018-06-29 16:49:10 +0200

    -

--- Rscripts/evaluate_results/plot_facets_of_clustering_tSNE.R
+++ Rscripts/evaluate_results/plot_facets_of_clustering_tSNE.R
@@ -1,6 +1,6 @@
 #tSNE plots
 
-DIR <- ""
+DIR <- "data/"
 FILTERINGS  <-  c("Expr", "M3Drop" ,"HVG")
 PCTKEEP  <-  c("10")
 DATASETS <-  c("Kumar", "Trapnell" ,"Koh" ,"SimKumar4easy" ,"SimKumar4hard", "SimKumar8hard", "KohTCC", "TrapnellTCC" ,"KumarTCC","Zhengmix4eq" ,"Zhengmix4uneq" ,"Zhengmix8eq")
@@ -13,7 +13,6 @@ names(full_data) <- DATASETS_FILTERED
 res_summary <- readRDS(file="output/clustering_summary/clustering_summary.rds")
 
 #plot tSNE
-scater::plotTSNE(res, colour_by = "phenoid", title=NULL) 
 plots.tsne <- plyr::laply(res, function(x)scater::plotTSNE(x, colour_by = "phenoid", title=NULL) )
 
 data <- c("sce_filteredExpr10_Zhengmix4eq", "sce_filteredExpr10_Zhengmix4eq","sce_filteredExpr10_Zhengmix4uneq", "sce_filteredExpr10_Trapnell")
@@ -33,8 +32,8 @@ plot_tSNE <- function(res_summary, full_data, meth,data,run){
                          colnames(sel.data),res$cluster,res$trueclass )
   p <- ggplot(df_to_plot, 
         aes(x = X1 , y = X2)) + 
-        xlab("dimanesion 1") + 
-        ylab("dimanesion 2") + 
+        xlab("dimension 1") + 
+        ylab("dimension 2") + 
         geom_rug(colour = "gray20", 
         alpha = 0.65) + 
         theme_bw() +
@@ -49,9 +48,7 @@ plot_tSNE <- function(res_summary, full_data, meth,data,run){
 }
 p.list <- lapply(c(1:4), function(x){
     plot_tSNE(res_summary, full_data, meth[x],data[x],nrun)
-} )
+})
 #save plots
-
 plot.grid <- cowplot::plot_grid(plotlist=p.list ,labels = "auto")
-
-cowplot::ggsave(plot.grid, filename = "bad_clustering.pdf", width=20, height=15)
+cowplot::ggsave(plot.grid, filename = "plot_facets_clustering.pdf", width=20, height=15)
