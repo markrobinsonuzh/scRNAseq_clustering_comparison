1fbeb34135fc4b93ef5711802f390140f25ba0a5
Charlotte Soneson <charlottesoneson@gmail.com>
2018-07-01 18:01:48 +0200

    Add figure4 script

---
+++ Rscripts/manuscript/plot_figure4.R
@@ -0,0 +1,21 @@
+args <- (commandArgs(trailingOnly = TRUE))
+for (i in 1:length(args)) {
+  eval(parse(text = args[[i]]))
+}
+
+print(consensusrds)
+print(outrds)
+
+suppressPackageStartupMessages({
+  library(cowplot)
+})
+
+consensus <- readRDS(consensusrds)
+
+pdf(gsub("rds$", "pdf", outrds), width = 20, height = 10)
+cowplot::plot_grid(plotlist = sapply(consensus[1], '[', 3))
+dev.off()
+
+saveRDS(NULL, file = outrds)
+date()
+sessionInfo()
